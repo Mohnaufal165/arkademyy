9332061512dd08bdd8c4019ff23c18b514e17a64
mohnaufal165 <Pangestyl165@gmail.com>
2020-05-09 20:55:01 +0700

    fixing no1

--- no1.js
+++ no1.js
@@ -8,10 +8,9 @@ function Biodata(name, age) {
         ],
         is_married: false,
         list_school: [
-            {name: "SMA X"},
-            {year_in: "2017"},
-            {year_out: "2020"},
-            {major: "IPA"},
+            {name: "SMAN 1 SETU",year_in: "2017",year_out: "2020",major: "IPA"},
+            {name: "SMP FAJAR DUNIA",year_in: "2014",year_out: "2017",major: ""},
+            {name: "SD PRATAMA",year_in: "2007",year_out: "2014",major: ""}
         ],
         skills: [
             "Outdoor activity",
